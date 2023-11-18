f89340606a6a3b6e064c61f106be69ad0ba71e1a
Daniel_Kimani_Muthoni-at-819587518695 <dandevopsgratitudeindia.com>
2023-11-18 16:33:36 +0300

    HackthonAI

--- hackthon/routes/web.php
+++ hackthon/routes/web.php
@@ -14,7 +14,7 @@ use App\Mail\Recommendation;
 */
 
 Route::get('/', function () {
-    Mail::to('kimdan2030@gmail.com')->send(new Recommendation());
+    Mail::to('keemdan345@gmail.com')->send(new Recommendation());
     return view('welcome');
 });
 
