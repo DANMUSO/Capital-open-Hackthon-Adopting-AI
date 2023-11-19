571e89340b707c51f79d9ba501cba471b3ff488e
Daniel_Kimani_Muthoni-at-819587518695 <dandevopsgratitudeindia.com>
2023-11-19 07:38:41 +0300

    HackthonAI

--- hackthon/resources/views/home.blade.php
+++ hackthon/resources/views/home.blade.php
@@ -246,7 +246,7 @@
 						<h2 style="color:#fff">Trending Movies</h2>
 					</div>
 				</div>
-				<hr style="color:#fff">
+				<br>
 				<!-- Feature Block -->
 				<div class="feature-block">
 					<div class="inner-box">
